b07a9e5159b8d7a00112f92af4b32b77cba14612
ascriver <ascriver@live.com>
2018-12-05 00:49:58 -0700

    another attempt

---
+++ README.md
@@ -0,0 +1,5 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## How To Play:
+
+Try to click each character only once! The DEA will know you're lying if you can't keep your story straight. Get 12 points and you win!
---
+++ src/App.js
@@ -0,0 +1,112 @@
+import React from "react";
+import FriendCard from "./components/FriendCard";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
+import friends from "./friends.json";
+import "./App.css";
+
+// https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array/6274398
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+// By extending the React.Component class, App inherits functionality from it
+class App extends React.Component {
+
+  // Set this.state
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "Heisenberg?" });
+    }
+
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Caught by the DEA!",
+      clicked: []
+    });
+    
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Nav
+          title="Breaking Bad Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
+
+        <Title>
+          Click each character only once!
+        </Title>
+
+        <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              <Column size="md-3 sm-6">
+                <FriendCard
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
